f366c87f7e4e9e97393230784075a079fa4e7154
Izomorf <Izomorf@gmail.com>
2019-10-08 00:14:51 +0300

    Removed .idea from index. Updated gitignore. Fixed a bug in omsi.php

--- system/library/omsi/omsi.php
+++ system/library/omsi/omsi.php
@@ -1,4 +1,21 @@
 <?php
   require_once 'util/globalConstants.php';
   require_once 'util/mysql.php';
-  require_once 'processing/ProductsDbHelper.php';
+  require_once 'helper/ProductsDbHelper.php';
+
+  class Omsi
+  {
+      private static $instance;
+
+      /**
+       * @param object $registry Registry Object
+       */
+      public static function get_instance($registry)
+      {
+          if (is_null(static::$instance)) {
+              static::$instance = new static($registry);
+          }
+
+          return static::$instance;
+      }
+  }
